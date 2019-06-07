2f31d3defad75781080ab4d42d91d184e3857bb0
Nanda Okitavera <codeharuka.yusa@gmail.com>
2019-06-07 20:52:58 +0700

    techpack/audio: fix soundwire and audio calibration init if !MODULES
    Signed-off-by: Nanda Okitavera <codeharuka.yusa@gmail.com>
    Change-Id: I738cb283eea790a61cc2663b97cf2b3d88759d9b
    [kras: Don't break MODULES]
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- techpack/audio/soc/soundwire.c
+++ techpack/audio/soc/soundwire.c
@@ -1117,7 +1117,11 @@ static int __init soundwire_init(void)
 
 	return retval;
 }
+#ifdef MODULE
 module_init(soundwire_init);
+#else
+postcore_initcall(soundwire_init);
+#endif
 module_exit(soundwire_exit);
 
 
