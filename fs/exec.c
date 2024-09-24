6052777c78160fad7903cf8d578b5acb6694bd45
LeCmnGend <namhoang1235@gmail.com>
2024-09-24 16:09:15 +0700

    ksu: Bringup again

--- fs/exec.c
+++ fs/exec.c
@@ -1738,6 +1738,11 @@ static void android_service_blacklist(const char *name)
 	}
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1746,6 +1751,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif	
+	
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
